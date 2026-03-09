7beac4516fb6928acaaf3d1b63197364ce7e33f0
Rawynn <rawynn@gmail.com>
2026-03-09 05:11:07 +0100

    books-library plugin scaffold

---
+++ wp-content/plugins/books-library/assets/js/scripts.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Books Library assets loaded");
+});
---
+++ wp-content/plugins/books-library/books-library.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Books Library
+ * Description: Recruitment task solution for Books CPT, Genre taxonomy, templates, AJAX and FAQ block.
+ * Version: 1.0.0
+ * Author: JB
+ * Text Domain: books-library
+ */
+
+declare(strict_types=1);
+
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+define('BOOKS_LIBRARY_VERSION', '1.0.0');
+define('BOOKS_LIBRARY_PATH', plugin_dir_path(__FILE__));
+define('BOOKS_LIBRARY_URL', plugin_dir_url(__FILE__));
