cf27b0c235277b39b08e2751fb1c7f69182aeed7
Rawynn <rawynn@gmail.com>
2026-03-09 11:12:12 +0100

    add single book template

---
+++ wp-content/plugins/books-library/includes/class-templates.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class Books_Library_Templates {
+
+    public static function init(): void {
+        add_filter('single_template', [self::class, 'load_single_book_template']);
+    }
+
+    public static function load_single_book_template(string $template): string {
+
+        if (is_singular('book')) {
+
+            $plugin_template = BOOKS_LIBRARY_PATH . 'templates/single-book.php';
+
+            if (file_exists($plugin_template)) {
+                return $plugin_template;
+            }
+        }
+
+        return $template;
+    }
+}
